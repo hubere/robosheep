296a4ebb41071de72d562d0e2bff0c8e359801ef
Edwin Huber <Edwin Huber>
2016-11-18 23:02:55 +0100

    Motorshield with Webserver

--- sketchbook/Motordriver/DualMC33926MotorShield.h
+++ sketchbook/Motordriver/DualMC33926MotorShield.h
@@ -1,67 +1,75 @@
 #ifndef DualMC33926MotorShield_h
 #define DualMC33926MotorShield_h
 
-//#include <Arduino.h>
+// see https://iotbytes.wordpress.com/nodemcu-pinout/
+
+#define D0  16
+#define D1  5
+#define D2  4
+#define D3  0
+#define D4  2
+#define D5  14
+#define D6  12
+#define D7  13
+#define D8  15
+
+#define SD2  9
+#define SD3  10
+
 
 class DualMC33926MotorShield
 {
   public:  
-// Constructors ////////////////////////////////////////////////////////////////
+  // Constructors ////////////////////////////////////////////////////////////////
 
-DualMC33926MotorShield()
-{
-  //Pin map
-  _nD2 = 4;
-  _M1DIR = 7;
-  _M2DIR = 8;
-  _nSF = 12;
-  _M1FB = 0; 
-  _M2FB = 1;
-}
+  DualMC33926MotorShield()
+  {
+    //Pin map
+    _M1DIR = D0;
+    _M1PWM = D1;
+    _M1FB =  D2; 
 
-DualMC33926MotorShield(unsigned char M1DIR, unsigned char M1PWM, unsigned char M1FB,
-                                               unsigned char M2DIR, unsigned char M2PWM, unsigned char M2FB,
-                                               unsigned char nD2, unsigned char nSF)
-{
-  //Pin map
-  //PWM1 and PWM2 cannot be remapped because the library assumes PWM is on timer1
-  _nD2 = nD2;
-  _M1DIR = M1DIR;
-  _M2DIR = M2DIR;
-  _nSF = nSF;
-  _M1FB = M1FB; 
-  _M2FB = M2FB;
-}
+    _M2DIR = D5;
+    _M2PWM = D6;
+    _M2FB =  D7;
+    
+    _nD2 =   D4;
+    _nSF =   D8;
+  }
 
-// Public Methods //////////////////////////////////////////////////////////////
-void init()
-{
-// Define pinMode for the pins and set the frequency for timer1.
 
-  pinMode(_M1DIR,OUTPUT);
-  pinMode(_M1PWM,OUTPUT);
-  pinMode(_M1FB,INPUT);
-  pinMode(_M2DIR,OUTPUT);
-  pinMode(_M2PWM,OUTPUT);
-  pinMode(_M2FB,INPUT);
-  pinMode(_nD2,OUTPUT);
-  digitalWrite(_nD2,HIGH); // default to on
-  pinMode(_nSF,INPUT);
+  // Public Methods //////////////////////////////////////////////////////////////
+  void init()
+  {
+    // Define pinMode for the pins and set the frequency for timer1.
+    pinMode(_M1DIR,OUTPUT);
+    Serial.println("pinMode(_M1DIR (D0),OUTPUT)"); delay(10);    
+    
+    pinMode(_M1PWM,OUTPUT);
+    Serial.println("pinMode(_M1PWM (D1),OUTPUT)"); delay(10);    
+    
+    pinMode(_M1FB,INPUT);
+    Serial.println("pinMode(_M1FB  (D2),INPUT)"); delay(10);    
+    
+    pinMode(_M2DIR,OUTPUT);
+    Serial.println("pinMode(_M2DIR (D5),OUTPUT)"); delay(10);    
+    
+    pinMode(_M2PWM,OUTPUT);
+    Serial.println("pinMode(_M2PWM (D6),OUTPUT)"); delay(10);    
+    
+    pinMode(_M2FB,INPUT);
+    Serial.println("pinMode(_M2FB  (D7),INPUT)"); delay(10);    
+    
+    pinMode(_nD2,OUTPUT);
+    Serial.println("pinMode(_nD2 (D4),OUTPUT)"); delay(10);    
+    
+    pinMode(_nSF,INPUT);
+    Serial.println("pinMode(_nSF (D8),OUTPUT)"); delay(10);    
 
-  #if defined(__AVR_ATmega168__)|| defined(__AVR_ATmega328P__)
-  // Timer 1 configuration
-  // prescaler: clockI/O / 1
-  // outputs enabled
-  // phase-correct PWM
-  // top of 400
-  //
-  // PWM frequency calculation
-  // 16MHz / 1 (prescaler) / 2 (phase-correct) / 400 (top) = 20kHz
-  TCCR1A = 0b10100000;
-  TCCR1B = 0b00010001;
-  ICR1 = 400;
-  #endif
-}
+    Serial.println("digitalWrite(_nD2,HIGH)"); delay(10);    
+    
+  }
+
 // Set speed for motor 1, speed is a number betwenn -400 and 400
 void setM1Speed(int speed)
 {
@@ -74,11 +82,7 @@ void setM1Speed(int speed)
   }
   if (speed > 400)  // Max PWM dutycycle
     speed = 400;
-  #if defined(__AVR_ATmega168__)|| defined(__AVR_ATmega328P__)
-  OCR1A = speed;
-  #else
-  analogWrite(_M1PWM,speed * 51 / 80); // default to using analogWrite, mapping 400 to 255
-  #endif
+  analogWrite(_M1PWM,speed * 51 / 800); // default to using analogWrite, mapping 400 to 255
   if (reverse)
     digitalWrite(_M1DIR,HIGH);
   else
@@ -97,11 +101,7 @@ void setM2Speed(int speed)
   }
   if (speed > 400)  // Max PWM dutycycle
     speed = 400;
-  #if defined(__AVR_ATmega168__)|| defined(__AVR_ATmega328P__)
-  OCR1B = speed;
-  #else
-  analogWrite(_M2PWM,speed * 51 / 80); // default to using analogWrite, mapping 400 to 255
-  #endif
+  analogWrite(_M2PWM,speed * 51 / 800); // default to using analogWrite, mapping 400 to 255
   if (reverse)
     digitalWrite(_M2DIR,HIGH);
   else
@@ -139,8 +139,8 @@ unsigned char getFault()
     unsigned char _nD2;
     unsigned char _M1DIR;
     unsigned char _M2DIR;
-    static const unsigned char _M1PWM = 9;
-    static const unsigned char _M2PWM = 10;
+    unsigned char _M1PWM = 9;
+    unsigned char _M2PWM = 10;
     unsigned char _nSF;
     unsigned char _M1FB;
     unsigned char _M2FB;
--- sketchbook/Motordriver/Motordriver.ino
+++ sketchbook/Motordriver/Motordriver.ino
@@ -1,48 +1,195 @@
-
+/*
+ * Motordriver:  
+ *  
+ * Functional description:
+ *   - on start device connects to WiFi and initialises MotorShield
+ *   - it listens on WIFI TODO FIXME HU
+ *   
+ *   
+ *   - when button is pressed, LED is flashed for 0.25 seconds and order 
+ *     message is published
+ *   - when status message from backend is received, LED is flashed 
+ *     for 2 seconds to give the user feedback that the order is processed.
+ * -------------------------------------------------------------------------
+ * This is DEMO source code, demonstrating some basics and principles
+ * used in IoT devices (e.g. MQTT, interfacing with hardware input and 
+ * outputs). In a real world application we would likely use a REST 
+ * interface to the backend service and also need some kind of personal-
+ * isation procedure to initialize the device. Also note that for reasons
+ * of simplicity that this demo does not implement any security measures 
+ * like encryption, authentication or authorization.
+ * -------------------------------------------------------------------------
+ * 
+ * see (https://learn.sparkfun.com/tutorials/esp8266-thing-hookup-guide/using-the-arduino-addon)
+ * 
+ */
 
 
 #include "DualMC33926MotorShield.h"
+#include <ESP8266WiFi.h>
 
-DualMC33926MotorShield md;
 
+
+// WiFi connection configuration
+//#define WIFI_PASSWORD   "LeInternetUwant11"
+//#define WIFI_SSID       "Buergernetz-WLAN"
+#define WIFI_PASSWORD   "Welpenspiel"
+#define WIFI_SSID       "TP-Huabas"
+// maximum number of tries during wifi connection setup
+#define WIFI_MAX_TRIES  30
+// delay in ms between checks during wifi connection setup
+#define WIFI_DELAY_TRY  1000
+
+
+/////////////////////
+// Pin Definitions //
+/////////////////////
+const int LED_PIN = 5; // Thing's onboard, green LED
+const int ANALOG_PIN = A0; // The only analog pin on the Thing
+const int DIGITAL_PIN = 12; // Digital pin to be read
 const int ledPin = 13;
+
+
+//
+// global variables
+// 
 int speedM1 = 0;
 int speedM2 = 0;
+int loopCount = 0;
+
+WiFiServer server(80);
+DualMC33926MotorShield md;
+
 
 void setup()
 {
- pinMode(ledPin, OUTPUT);
- Serial.begin(9600);
+  Serial.begin(115200);
+  delay(10);
+  Serial.println();
+  Serial.println();
+  Serial.println();
 
-  Serial.println("Dual MC33926 Motor Shield");
+  // prepare GPIO
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, 0);
+  delay(10);
+
+  Serial.println("\nInitializing Dual MC33926 Motor Shield");
   md.init();
+  delay(10);
+
+  Serial.println("\nInitializing Wifi");
+  connectWiFi();
+  delay(10);
+
+  Serial.println("\nStarting Webserver");
+  server.begin();
+  delay(10);
+
+  Serial.println("Usage:");
+  Serial.println("");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/sheep?speed=<speed>&dir=<dir>");
+  Serial.println("");
+  Serial.println("where");
+  Serial.println("  <speed> : -400 to 400");
+  Serial.println("  <dir>   : |speed| + |dir| < 400");
+  Serial.println("");
+
 }
 
 void loop()
 {
-  if (Serial.available()){
-    int cmdSpeed = Serial.parseInt();
-    int cmdDir = Serial.parseInt();
-  
-    Serial.print("received speed: ");
-    Serial.print(cmdSpeed);
-    Serial.print(" dir: ");
-    Serial.println(cmdDir);
-
-    adjustSpeedM1(cmdSpeed + cmdDir);
-    adjustSpeedM2(cmdSpeed - cmdDir);
-    
-    Serial.print("M1: ");
-    Serial.print(md.getM1CurrentMilliamps());
-    Serial.print(" M2: ");
-    Serial.println(md.getM2CurrentMilliamps());
-  
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    Serial.print(".");
+    loopCount = loopCount+1;
+    if (loopCount > 50)
+    {
+      loopCount =0;
+      Serial.println();       
+    }
+    delay(1000);    
+    return;
   }
- delay(1000);
+  loopCount =0;
+
+  // Wait until the client sends some data
+  Serial.println("\n\nnew client");
+  while(!client.available()){
+    delay(1);
+  }
+
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  Serial.println(req);
+  client.flush();
+  
+  // Match the request
+  int cmdSpeed = 0;
+  int cmdDir = 0;
+
+  if (req.indexOf("/sheep") != -1){
+    /*
+    int locationQuestionmark = req.find("?");
+    String params = req.substr(locationQuestionmark+1, req.size());
+    int locationEquals = req.find("=");
+    String speedString = req.substr(locationEquals+1, req.size());
+    int cmdSpeed = std::stoi(speedString);
+    */
+
+    int posSpeed = req.indexOf("speed=");
+    int posDir = req.indexOf("dir=");
+    if (posSpeed != -1)
+    {
+      String speedString = req.substring(posSpeed+6, req.length());
+      cmdSpeed =  speedString.toInt();
+    }
+    if (posDir != -1)
+    {
+      String dirString = req.substring(posDir+4, req.length());
+      cmdDir =  dirString.toInt();
+    }
+
+  }
+  else {
+    Serial.println("invalid request");
+    client.stop();
+    return;
+  }
+
+  adjustSpeedM1(cmdSpeed + cmdDir);
+  adjustSpeedM2(cmdSpeed - cmdDir);
+
+  Serial.print("M1: ");
+  Serial.print(md.getM1CurrentMilliamps());
+  Serial.print(" M2: ");
+  Serial.println(md.getM2CurrentMilliamps());
+
+  client.flush();
+
+  // Prepare the response
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nSpeed-dir is now ";
+  s += md.getM1CurrentMilliamps();
+  s += " - ";
+  s += md.getM2CurrentMilliamps();
+  s += "</html>\n";
+
+  // Send the response to the client
+  client.print(s);
+  delay(1);
+  Serial.println("Client disonnected");
+
+  // The client will actually be disconnected 
+  // when the function returns and 'client' object is detroyed
+  
+  // delay(1000);
 }
 
 void adjustSpeedM1(int newSpeed){
-  Serial.print("M1 ");
+  Serial.print("adjustSpeedM1 to  ");
   Serial.println(newSpeed);
   if (newSpeed < speedM1){
      decelerate("M1", speedM1, newSpeed*4); 
@@ -53,7 +200,7 @@ void adjustSpeedM1(int newSpeed){
 }
 
 void adjustSpeedM2(int newSpeed){
-  Serial.print("M2 ");
+  Serial.print("adjustSpeedM2 to ");
   Serial.println(newSpeed);
   if (newSpeed < speedM2){
      decelerate("M2", speedM2, newSpeed*4);
@@ -85,6 +232,27 @@ void decelerate(String motor, int from, int to){
     }
     delay(2);
   }
+}
+
+
+void connectWiFi()
+{
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+  
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
 }
 
 
