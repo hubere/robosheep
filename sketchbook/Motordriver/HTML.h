44017a203d167cd471ef6a057ebaf51790a82d23
Edwin Huber <Edwin Huber>
2016-11-27 11:54:43 +0100

    HTML Framework

---
+++ sketchbook/Motordriver/HTML.h
@@ -0,0 +1,18 @@
+#ifndef HTML_h
+#define HTML_h
+
+/**
+ * 1) Write html-code
+ * 2) remove all inline comments starting with '//'
+ * 3) Replace '\r' with '\ \r'
+ * 4) Replace '"' with '\"'
+ * 5) Copy & paste the code into '<here comes the html>'
+ */
+
+
+const char page1 = "\
+<here comes the html>\
+";
+
+
+#endif
