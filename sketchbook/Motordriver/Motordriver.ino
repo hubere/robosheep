320b28fb5091015a2417050022c4ae8eb893dc86
Edwin Huber <Edwin.Huber@bmw.de>
2020-12-21 12:54:16 +0100

    FIX send one command per second at most. vertical status bars.

--- sketchbook/Motordriver/Motordriver.ino
+++ sketchbook/Motordriver/Motordriver.ino
@@ -109,7 +109,8 @@ float vin = 0.0;
 float R1 = 100000.0; // resistance of R1 (100K) -see text!
 float R2 = 10000.0; // resistance of R2 (10K) - see text!
 int value = 0;
-
+int batteryPowerMeasurements = 0;
+long summedBatteryPowerValue = 0;
 
 //
 // global objects
@@ -273,16 +274,26 @@ void toggleLED(int bigLoopCount)
 
 /*
  * Measure battery power
+ * (see https://www.engineersgarage.com/esp8266/nodemcu-battery-voltage-monitor/)
  * (see https://www.electroschematics.com/arduino-digital-voltmeter/)
  */
 void measureBatteryPower()
 {
-   // read the value at analog input
-   value = analogRead(ANALOG_PIN);
-   vout = (value * 5.0) / 1024.0; // see text
-   vin = vout / (R2/(R1+R2)); 
-   if (vin<0.09) vin=0.0;//statement to quash undesired reading !
-   batteryPower = vin;
+  const int POWER_READINGS_COUNT = 10;
+  
+  summedBatteryPowerValue += analogRead(ANALOG_PIN);    
+  batteryPowerMeasurements++;
+  if (batteryPowerMeasurements >= POWER_READINGS_COUNT)
+  {
+    value = summedBatteryPowerValue / POWER_READINGS_COUNT;
+    vout = (value * 3.3) / 1024.0; // Convert voltage to 5V factor
+    vout = (value * 3.1) / 1024.0; // 2.9 was empeeric factor!!!! No clue why!?!
+    vin = vout / (R2/(R1+R2)); 
+    if (vin<0.09) vin=0.0;//statement to quash undesired reading !
+    batteryPower = vin*10;
+    batteryPowerMeasurements = 0;
+    summedBatteryPowerValue = 0;
+  }
 }
 
 
